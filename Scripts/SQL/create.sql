8edf872a8f880e78eed431e1b28e4b92b0123d3c
Akram El-Gaouny <aelga098@uottawa.ca>
2022-02-09 17:56:49 -0500

    [CSI-2132][RelationalModel] Added intial table defs.

---
+++ Scripts/SQL/create.sql
@@ -0,0 +1,76 @@
+-- User
+CREATE TABLE User (
+
+)
+-- PhoneNumber
+CREATE TABLE PhoneNumber (
+
+)
+-- Patient
+CREATE TABLE Patient (
+
+)
+-- Branch
+CREATE TABLE Branch (
+
+)
+-- Review
+CREATE TABLE Review (
+
+)
+-- Employee
+CREATE TABLE Employee (
+
+)
+-- ResponsibleFor
+CREATE TABLE ResponsibleFor (
+
+)
+-- PatientChart
+CREATE TABLE PatientChart (
+
+)
+-- Authored
+CREATE TABLE Authored (
+
+)
+-- Invoice
+CREATE TABLE Invoice (
+
+)
+-- PaymentType
+CREATE TABLE PaymentType (
+
+)
+-- Appointment
+CREATE TABLE Appointment (
+
+)
+-- FeeCharge
+CREATE TABLE FeeCharge (
+
+)
+-- AppointmentProcedure
+CREATE TABLE AppointmentProcedure (
+
+)
+-- PatientPayment
+CREATE TABLE PatientPayment (
+
+)
+-- InsuranceClaim
+CREATE TABLE InsuranceClaim (
+
+)
+-- Treatment
+CREATE TABLE Treatment (
+
+)
+-- Symptom
+CREATE TABLE Symptom (
+
+)
+-- Medication
+CREATE TABLE Medication (
+
+)
