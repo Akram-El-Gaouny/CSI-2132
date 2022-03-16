5c8bcb7ef7ab97ce179b82d86f113192f9017914
Akram El-Gaouny <64698780+Akram-El-Gaouny@users.noreply.github.com>
2022-03-16 14:16:25 -0400

    [Query] Dentists in branch query added (#4)

--- Server/Routes/routes.js
+++ Server/Routes/routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import {health} from "../controllers/healthController.js";
-import {test} from "../controllers/appQueries.js";
+import {test, dentistsInBranch} from "../controllers/appQueries.js";
 const Router = express.Router();
 
 // get requests
 Router.get("/health", health);
 Router.get("/test", test);
+Router.get("/dentistsInBranch", dentistsInBranch)
 
 export default Router;
--- Server/controllers/appQueries.js
+++ Server/controllers/appQueries.js
@@ -5,7 +5,6 @@ export function test(req, res) {
 
   console.log("A request has been made to the /test endpoint");
   SqlConnection.query(query, function (err, result, fields) {
-    
     if (err) {
       res.status(500).json(err);
       console.error(err);
@@ -17,7 +16,43 @@ export function test(req, res) {
       return;
     }
 
-    res.json({"queryResults": result});
-
+    res.json({ queryResults: result });
   });
+}
+
+// req handler that returns a list of dentists
+export function dentistsInBranch(req, res) {
+  let branchid = req.query.branchid;
+  if (branchid === null) {
+    res.status(400).json({message: "Please provide a branch id"});
+    return;
+  }
+
+  let query = `
+        SELECT first, last FROM User
+        WHERE User.userID IN (
+          SELECT employee.employeeID FROM Employee
+          WHERE employee.position = "dentist" and branchID = "${branchid}"
+        );
+  
+  `;
+
+  console.log("A request has been made to /dentistsInBranch endpoint");
+
+  SqlConnection.query(query, function (err, result, fields) {
+   
+    if (err) {
+      res.status(500).json(err);
+      console.error("An error occurred at the dentistsInBranch endpoint,", "Error Code: " + err.code +",", "Error Message: " + err.sqlMessage);
+      return;
+    }
+
+    if (result.length === 0) {
+      res.json({ message: "no matches found" });
+      return;
+    }
+
+    res.json({ queryResults: result });
+  });
+
 }
