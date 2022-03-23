16e33ace487e96475896c8d4c5cb0adc30c9329f
Akram El-Gaouny <64698780+Akram-El-Gaouny@users.noreply.github.com>
2022-03-23 14:02:29 -0400

    Made nav bar more flexible (#5)

--- db-project/src/App.js
+++ db-project/src/App.js
@@ -1,16 +1,38 @@
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <>
+
       <Router>
-        <Navbar/>
+
+      <Switch>
+
+        <Route exact path = "/">
+        <Navbar List = {[ { display : "Branches", path : "/"}, { display : "Sign In", path : "/signin"},  { display : "Sign Up", path : "/"}]}/>
         <Footer/>
+        </Route>
+
+        <Route exact path = "/signin">
+        <Navbar List = {[ { display : "Home", path : "/"} , { display : "Sign Up", path : "/"}]}/>
+        <Footer/>
+        </Route>
+
+        <Route exact path = "/*">
+        <Redirect to = {'/'} />
+        </Route>
+
+
+     
+      </Switch>
+
+        
+
       </Router>
-    </>
+    
+
   );
 }
 
