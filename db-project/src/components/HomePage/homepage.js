1f5e5e0162d17dc9ca60e2537eb145235c5ea779
Akram El-Gaouny <aelga098@uottawa.ca>
2022-03-30 14:20:26 -0400

    Fix issue with homepage

--- db-project/src/components/HomePage/homepage.js
+++ db-project/src/components/HomePage/homepage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './homepage.css';
+import background from "../../images/dentist.jpeg";
 
 function HomePage() {
   return (
     <div className='hero-container'>
-      <img src='images/dentist.jpeg' alt='' />
+      <img src= {background} alt='' />
       <h1>Dentist Clinic</h1>
       <div className='hero-btns'>
       <a href="/CSI-2132/branches" className='btn' >Branches</a>
