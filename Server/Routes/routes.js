9c25559fc1806fdb37c211c051197415a4edb6b0
Akram El-Gaouny <aelga098@uottawa.ca>
2022-03-30 16:27:10 -0400

    Added new endpoints

--- Server/Routes/routes.js
+++ Server/Routes/routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import {health} from "../controllers/healthController.js";
-import {authenticate, dentistsInBranch} from "../controllers/appQueries.js";
+import {authenticate, dentistsInBranch, appointmentsByDentist, appointmentsByPatient, feesDescriptions} from "../controllers/appQueries.js";
 const Router = express.Router();
 
 // get requests
 Router.get("/health", health);
 Router.get("/authenticate", authenticate);
 Router.get("/dentistsInBranch", dentistsInBranch)
-
+Router.get("/appointmentsByDentist", appointmentsByDentist)
+Router.get("/appointmentsByPatient", appointmentsByPatient)
+Router.get("/feeDescriptions", feesDescriptions)
 export default Router;
--- Server/controllers/appQueries.js
+++ Server/controllers/appQueries.js
@@ -1,17 +1,21 @@
+import e from "express";
 import { SqlConnection } from "../server.js";
 
 export function authenticate(req, res) {
-
-  let email, password, position = null;
+  let email,
+    password,
+    position = null;
   email = req.query.email;
   password = req.query.password;
   position = req.query.position;
   role = req.query.role;
-  if (email == "" || password == "" || position == "" || role == "") {
-    res.status(400).json({message: "Please make sure you have supplied an email address, password"});
+  if (email === "" || password === "" || position === "" || role === "") {
+    res.status(400).json({
+      message: "Please make sure you have supplied an email address, password",
+    });
     return;
   }
-  
+
   let query = `SELECT userID, first, last, position
   FROM user JOIN employee AS E
   WHERE userID=employeeID and email = "${email}"and password ="${password}" and ( position = "${position}" OR role = "${role}");`;
@@ -25,7 +29,7 @@ export function authenticate(req, res) {
     }
 
     if (result.length === 0) {
-      res.status(404).json({ message: "No matches found"});
+      res.status(404).json({ message: "No matches found" });
       return;
     }
 
@@ -35,11 +39,10 @@ export function authenticate(req, res) {
 
 // req handler that returns a list of dentists
 export function dentistsInBranch(req, res) {
-  
   let branchid = req.query.branchid;
-  
+
   if (branchid === null) {
-    res.status(400).json({message: "Please provide a branch id"});
+    res.status(400).json({ message: "Please provide a branch id" });
     return;
   }
 
@@ -55,10 +58,13 @@ export function dentistsInBranch(req, res) {
   console.log("A request has been made to /dentistsInBranch endpoint");
 
   SqlConnection.query(query, function (err, result, fields) {
-   
     if (err) {
       res.status(500).json(err);
-      console.error("An error occurred at the dentistsInBranch endpoint,", "Error Code: " + err.code +",", "Error Message: " + err.sqlMessage);
+      console.error(
+        "An error occurred at the dentistsInBranch endpoint,",
+        "Error Code: " + err.code + ",",
+        "Error Message: " + err.sqlMessage
+      );
       return;
     }
 
@@ -69,5 +75,110 @@ export function dentistsInBranch(req, res) {
 
     res.json({ queryResults: result[0] });
   });
+}
 
+export function appointmentsByDentist(req, res) {
+  let employeeID = null;
+  employeeID = req.query.employeeID;
+
+  if (employeeID == null) {
+    res.status(400).json({ message: "Please provide a dentist id" });
+    return;
+  }
+
+  let query = `
+    SELECT * 
+    FROM Appointment 
+    WHERE employeeID = ${employeeID}
+    `;
+
+  SqlConnection.query(query, function (err, result, fields) {
+    if (err) {
+      res.status(500).json(err);
+      console.error(
+        "An error occurred at the appointmentByDentist endpoint,",
+        "Error Code: " + err.code + ",",
+        "Error Message: " + err.sqlMessage
+      );
+      return;
+    }
+
+    if (result.length === 0) {
+      res.json({ message: "no matches found" });
+      return;
+    }
+
+    res.json({
+      queryResults: result,
+      message: "Here are the appointment results",
+    });
+  });
+}
+
+export function appointmentsByPatient(req, res) {
+  let patientID = null;
+  patientID = req.query.patientID;
+
+  if (patientID === null) {
+    res.status(400).json({ message: "Please provide a branch id" });
+    return;
+  }
+
+  let query = `
+    SELECT * 
+    FROM Appointment 
+    WHERE patientID = ${patientID}
+    `;
+
+  SqlConnection.query(query, function (err, result, fields) {
+    if (err) {
+      res.status(500).json(err);
+      console.error(
+        "An error occurred at the appointmentByPatient endpoint,",
+        "Error Code: " + err.code + ",",
+        "Error Message: " + err.sqlMessage
+      );
+      return;
+    }
+
+    if (result.length === 0) {
+      res.json({ message: "no matches found" });
+      return;
+    }
+
+    res.json({
+      queryResults: result,
+      message: "Here are the appointment results",
+    });
+  });
+}
+
+export function feesDescriptions(req, res) {
+  let query = `
+    SELECT feeDesc 
+    FROM FeeCharge 
+    WHERE feecode != 94303; 
+    `;
+
+  SqlConnection.query(query, function (err, result, fields) {
+    if (err) {
+      res.status(500).json(err);
+      console.error(
+        "An error occurred at the feeDescriptions endpoint,",
+        "Error Code: " + err.code + ",",
+        "Error Message: " + err.sqlMessage
+      );
+      return;
+    }
+
+    if (result.length === 0) {
+      res.json({ message: "no matches found" });
+      return;
+    }
+
+    res.json({
+      queryResults: result,
+      message: "Here are the available results",
+    });
+  });
 }
