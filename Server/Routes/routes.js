5ee460d13cd3560867f73cd7dc4aa79a5237b071
Akram El-Gaouny <aelga098@uottawa.ca>
2022-04-04 13:25:38 -0400

    testing controllers

--- Server/Routes/routes.js
+++ Server/Routes/routes.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import {health} from "../controllers/healthController.js";
-import {authenticate, dentistsInBranch, appointmentsByDentist, appointmentsByPatient, feesDescriptions, addUser, addPhone, addPatientInsurance} from "../controllers/appQueries.js";
+import {authenticatePatient,authenticateEmployee, dentistsInBranch, appointmentsByDentist, appointmentsByPatient, feesDescriptions, addUser, addPhone, addPatientInsurance} from "../controllers/appQueries.js";
 const Router = express.Router();
 
 // get requests
 Router.get("/health", health);
-Router.get("/authenticate", authenticate);
+Router.get("/authenticateEmployee", authenticateEmployee);
+Router.get("/authenticatePatient", authenticatePatient);
 Router.get("/dentistsInBranch", dentistsInBranch)
 Router.get("/appointmentsByDentist", appointmentsByDentist)
 Router.get("/appointmentsByPatient", appointmentsByPatient)
 Router.get("/feeDescriptions", feesDescriptions)
 Router.get("/addUser", addUser)
-
 Router.put("/addPhone", addPhone)
 Router.put("/addPatientInsurance", addPatientInsurance)
 
--- Server/controllers/appQueries.js
+++ Server/controllers/appQueries.js
@@ -1,25 +1,63 @@
 import e from "express";
 import { SqlConnection } from "../server.js";
 
-export function authenticate(req, res) {
+export function authenticatePatient(req, res) {
   let email,
     password,
-    role,
-    position = null;
+    position = undefined;
+
   email = req.query.email;
   password = req.query.password;
-  position = req.query.position;
-  role = req.query.role;
-  if (email === "" || password === "" || position === "" || role === "") {
+
+  if (email === undefined || password === undefined) {
     res.status(400).json({
       message: "Please make sure you have supplied an email address, password",
     });
     return;
   }
 
-  let query = `SELECT userID, first, last, position
-  FROM user JOIN employee AS E
-  WHERE userID=employeeID and email = "${email}"and password ="${password}" and ( position = "${position}" OR role = "${role}");`;
+  let query = `
+  SELECT userID, first, last, role
+  FROM user 
+  WHERE email = "${email}"and password ="${password}" and role = "patient";`;
+
+  console.log("A request has been made to the /authenticate endpoint");
+  SqlConnection.query(query, function (err, result, fields) {
+    if (err) {
+      res.status(500).json(err);
+      console.error(err);
+      return;
+    }
+
+    if (result.length === 0) {
+      res.status(404).json({ message: "No matches found" });
+      return;
+    }
+
+    res.json({ queryResults: result });
+  });
+}
+
+export function authenticateEmployee(req, res) {
+  let email,
+    password,
+    position = undefined;
+
+  email = req.query.email;
+  password = req.query.password;
+  position = req.query.position;
+
+  if (email === undefined || password === undefined || position === undefined) {
+    res.status(400).json({
+      message: "Please make sure you have supplied an email address, password",
+    });
+    return;
+  }
+
+  let query = `
+  SELECT userID, first, last, position
+  FROM user AS u , employee AS E
+  WHERE u.userID=e.employeeID and u.email = "${email}" and u.password ="${password}" and e.position = "${position}" and u.role = "employee";`;
 
   console.log("A request has been made to the /authenticate endpoint");
   SqlConnection.query(query, function (err, result, fields) {
@@ -42,7 +80,7 @@ export function authenticate(req, res) {
 export function dentistsInBranch(req, res) {
   let branchid = req.query.branchid;
 
-  if (branchid === null) {
+  if (branchid === undefined) {
     res.status(400).json({ message: "Please provide a branch id" });
     return;
   }
@@ -79,10 +117,10 @@ export function dentistsInBranch(req, res) {
 }
 
 export function appointmentsByDentist(req, res) {
-  let employeeID = null;
+  let employeeID = undefined;
   employeeID = req.query.employeeID;
 
-  if (employeeID == null) {
+  if (employeeID == undefined) {
     res.status(400).json({ message: "Please provide a dentist id" });
     return;
   }
@@ -117,18 +155,19 @@ export function appointmentsByDentist(req, res) {
 }
 
 export function appointmentsByPatient(req, res) {
-  let patientID = null;
+  let patientID = undefined;
+
   patientID = req.query.patientID;
 
-  if (patientID === null) {
-    res.status(400).json({ message: "Please provide a branch id" });
+  if (patientID === undefined) {
+    res.status(400).json({ message: "Please provide a patient id" });
     return;
   }
 
   let query = `
     SELECT * 
     FROM Appointment 
-    WHERE patientID = ${patientID}
+    WHERE patientID = ${patientID};
     `;
 
   SqlConnection.query(query, function (err, result, fields) {
@@ -185,7 +224,18 @@ export function feesDescriptions(req, res) {
 }
 
 export function addUser(req, res) {
-  let fname, mname, lname, pemail, pass, hnum, str, cty, prov, role, dob, pssn = null;
+  let fname,
+    mname,
+    lname,
+    pemail,
+    pass,
+    hnum,
+    str,
+    cty,
+    prov,
+    role,
+    dob,
+    pssn = undefined;
   fname = req.query.fname;
   mname = req.query.mname;
   lname = req.query.lname;
@@ -199,64 +249,65 @@ export function addUser(req, res) {
   dob = req.query.dob;
   pssn = req.query.pssn;
 
-
-  if (fname === null) {
-      res.status(400).json({message: "Please provide a first name"});
-      return;
-  }
-  
-  if (lname === null) {
-      res.status(400).json({message: "Please provide a last name"});
-      return;
-  }
-
-  if (pemail === null) {
-      res.status(400).json({message: "Please provide an email"});
-      return;
-  }
-
-  if (pass === null) {
-      res.status(400).json({message: "Please provide a password"});
-      return;
-  }
-
-  if (hnum === null) {
-      res.status(400).json({message: "Please provide a house number"});
-      return;
-  }
-
-  if (str === null) {
-      res.status(400).json({message: "Please provide a street name"});
-      return;
-  }
-
-  if (cty === null) {
-      res.status(400).json({message: "Please provide a city"});
-      return;
-  }
-
-  if (prov === null) {
-      res.status(400).json({message: "Please provide a province"});
-      return;
-  }
-
-  if (role === null) {
-    res.status(400).json({message: "Please provide a role"});
+  if (fname === undefined) {
+    res.status(400).json({ message: "Please provide a first name" });
     return;
   }
 
-  if (dob === null) {
-      res.status(400).json({message: "Please provide a date of birth"});
-      return;
+  if (lname === undefined) {
+    res.status(400).json({ message: "Please provide a last name" });
+    return;
   }
 
-  if (pssn === null) {
-      res.status(400).json({message: "Please provide a social security number"});
-      return;
+  if (pemail === undefined) {
+    res.status(400).json({ message: "Please provide an email" });
+    return;
+  }
+
+  if (pass === undefined) {
+    res.status(400).json({ message: "Please provide a password" });
+    return;
+  }
+
+  if (hnum === undefined) {
+    res.status(400).json({ message: "Please provide a house number" });
+    return;
+  }
+
+  if (str === undefined) {
+    res.status(400).json({ message: "Please provide a street name" });
+    return;
+  }
+
+  if (cty === undefined) {
+    res.status(400).json({ message: "Please provide a city" });
+    return;
+  }
+
+  if (prov === undefined) {
+    res.status(400).json({ message: "Please provide a province" });
+    return;
+  }
+
+  if (role === undefined) {
+    res.status(400).json({ message: "Please provide a role" });
+    return;
+  }
+
+  if (dob === undefined) {
+    res.status(400).json({ message: "Please provide a date of birth" });
+    return;
+  }
+
+  if (pssn === undefined) {
+    res
+      .status(400)
+      .json({ message: "Please provide a social security number" });
+    return;
   }
 
   console.log("A request has been made to the /addUser endpoint");
-  
+
   let query = `
       INSERT INTO User
       (first, middle, last, email, password, houseNumber, street, city, province, role, dateOfBirth, ssn) 
@@ -265,32 +316,35 @@ export function addUser(req, res) {
   `;
 
   SqlConnection.query(query, function (err, result, fields) {
-
-  if (err) {
+    if (err) {
       res.status(500).json(err);
-      console.error("An error occurred at the addUser endpoint,", "Error Code: " + err.code +",", "Error Message: " + err.sqlMessage);
+      console.error(
+        "An error occurred at the addUser endpoint,",
+        "Error Code: " + err.code + ",",
+        "Error Message: " + err.sqlMessage
+      );
       return;
-  } else {
-    console.log("A user has successfully been added to the DB");
-    res.json( {uid: result.insertId} )
-  }
+    } else {
+      console.log("A user has successfully been added to the DB");
+      res.json({ uid: result.insertId });
+    }
   });
-  
 }
 
 export function addPhone(req, res) {
-  let uid, pnums = null;
+  let uid,
+    pnums = undefined;
   uid = req.query.uid;
   pnums = req.query.pnums;
 
-  if (uid === null) {
-      res.status(400).json({message: "Please provide a uid"});
-      return;
+  if (uid === undefined) {
+    res.status(400).json({ message: "Please provide a uid" });
+    return;
   }
 
   console.log("A request has been made to the /addPhone endpoint");
 
-  pnums.split(",").forEach(element => {
+  pnums.split(",").forEach((element) => {
     let query2 = `
       INSERT INTO PhoneNumber
       (userId, phoneNumber) 
@@ -298,57 +352,65 @@ export function addPhone(req, res) {
       (${uid}, ${element});
     `;
     SqlConnection.query(query2, function (err, result, fields) {
-
       if (err) {
-          res.status(500).json(err);
-          console.error("An error occurred at the addPatient endpoint,", "Error Code: " + err.code +",", "Error Message: " + err.sqlMessage);
-          return;
+        res.status(500).json(err);
+        console.error(
+          "An error occurred at the addPatient endpoint,",
+          "Error Code: " + err.code + ",",
+          "Error Message: " + err.sqlMessage
+        );
+        return;
       } else {
         console.log("A phone number has successfully been added to the DB");
       }
-      });
+    });
   });
 
   res.status(200);
-  
 }
 
 export function addPatientInsurance(req, res) {
-  let uid, ins = null;
+  let uid,
+    ins = undefined;
   uid = req.query.uid;
   ins = req.query.ins;
 
-  if (uid === null) {
-      res.status(400).json({message: "Please provide a uid"});
-      return;
+  if (uid === undefined) {
+    res.status(400).json({ message: "Please provide a uid" });
+    return;
   }
 
   console.log("A request has been made to the /addPatientInsurance endpoint");
 
-  let query3 = (ins == null ? `
+  let query3 =
+    ins == undefined
+      ? `
     INSERT INTO Patient
     (patientId, insuranceProvider) 
     VALUES 
-    (${uid}, null);
+    (${uid}, undefined);
   `
-  : 
-  `
+      : `
     INSERT INTO Patient
     (patientId, insuranceProvider) 
     VALUES 
     (${uid}, "${ins}");
-  `)
+  `;
   SqlConnection.query(query3, function (err, result, fields) {
-
     if (err) {
-        res.status(500).json(err);
-        console.error("An error occurred at the addPatient endpoint,", "Error Code: " + err.code +",", "Error Message: " + err.sqlMessage);
-        return;
+      res.status(500).json(err);
+      console.error(
+        "An error occurred at the addPatient endpoint,",
+        "Error Code: " + err.code + ",",
+        "Error Message: " + err.sqlMessage
+      );
+      return;
     } else {
-      console.log("A patient insurance provider has successfully been added to the DB");
+      console.log(
+        "A patient insurance provider has successfully been added to the DB"
+      );
     }
   });
 
   res.status(200);
-  
 }
